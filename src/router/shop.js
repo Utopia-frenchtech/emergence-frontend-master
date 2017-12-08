afccd94cbfaba4ee1c2936fe60c05d970310364f
Eric Burel <e.burel.contact@gmail.com>
2017-12-08 12:13:01 +0100

    Topbar title on cart

--- src/router/shop.js
+++ src/router/shop.js
@@ -37,7 +37,7 @@ const shopRoutes = [
     },
     props: {
       content: (route) => ({}),
-      top: (route) => ({})
+      top: (route) => ({i18n.t('components.shop.cart.topbar')})
     }
   },
   {
